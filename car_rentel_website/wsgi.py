b4aa89c5d536253003492c4704b27aa1a95f9392
syedfarazhussain <dishah4862@gmail.com>
2022-11-04 18:13:31 +0500

    change wsgi

--- car_rentel_website/wsgi.py
+++ car_rentel_website/wsgi.py
@@ -12,5 +12,6 @@ from whitenoise import WhiteNoise
 
 from django.core.wsgi import get_wsgi_application
 
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'car_rentel_website.settings')
 application = get_wsgi_application()
 application = WhiteNoise(application)
